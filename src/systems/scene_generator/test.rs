93a84cc2c725604b9b781432f5ea2cc141d890ce
Arawn <2300520400007@ietlucknow.ac.in>
2024-06-09 16:43:11 +0530

    Adding a Light component

---
+++ src/systems/scene_generator/test.rs
@@ -0,0 +1,47 @@
+use super::*;
+use crate::systems::TestHelper;
+
+type Subject = SceneGenerator;
+
+mod run {
+    use super::*;
+
+    #[test]
+    fn it_builds_a_camera() {
+        let mut t = TestHelper::new();
+
+        t.run(SceneGenerator);
+
+        let camera = t.entity("camera-1");
+
+        assert_eq!(t.has::<Camera>(camera), true);
+        assert_eq!(t.has::<Transform>(camera), true);
+        assert_eq!(t.has::<FieldOfView>(camera), true);
+        assert_eq!(t.has::<Image>(camera), true);
+    }
+
+    #[test]
+    fn it_builds_a_sphere() {
+        let mut t = TestHelper::new();
+
+        t.run(SceneGenerator);
+
+        let sphere = t.entity("sphere-1");
+
+        assert_eq!(t.has::<Sphere>(sphere), true);
+        assert_eq!(t.has::<Transform>(sphere), true);
+        assert_eq!(t.has::<Material>(sphere), true);
+    }
+
+    #[test]
+    fn it_builds_a_light() {
+        let mut t = TestHelper::new();
+
+        t.run(SceneGenerator);
+
+        let sphere = t.entity("light-1");
+
+        assert_eq!(t.has::<Light>(sphere), true);
+        assert_eq!(t.has::<Transform>(sphere), true);
+    }
+}
